eda52d4bf657aca16f93455e84c7d8a78b1930a8
LuizFelipeSptech <luiz.mendonca@sptech.school>
2025-03-09 16:43:01 -0300

    feat: Modificações feitas para demonstração de Login

--- src/main/java/com/aura8/general_backend/UsersRepository.java
+++ src/main/java/com/aura8/general_backend/UsersRepository.java
@@ -4,8 +4,11 @@ import com.aura8.general_backend.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
     List<Users> findAllByDeletedFalse();
+
+    Optional<Users> findByEmailAndPassword(String email, String password);
 }
