9cdee79d6e4e1a2ec66a13374b3f1037fdfbe83e
unknown <pablo.santana@sptech.school>
2025-05-31 00:22:22 -0300

    adicionando jwt e cors na jobController

--- src/main/java/com/aura8/general_backend/controller/JobController.java
+++ src/main/java/com/aura8/general_backend/controller/JobController.java
@@ -5,7 +5,9 @@ import com.aura8.general_backend.entities.Job;
 import com.aura8.general_backend.service.JobService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -24,17 +26,20 @@ public class JobController {
         this.service = service;
     }
 
+    @CrossOrigin(origins = "*")
+    @SecurityRequirement(name = "Bearer")
     @GetMapping("")
     @Operation(summary = "Listar serviços", description = "Retorna uma lista paginada de todos os serviços disponíveis")
     @ApiResponse(responseCode = "200", description = "Lista de serviços retornada com sucesso")
-    public ResponseEntity<Page<JobResponseDto>> getAll(Pageable pageable){
+    public ResponseEntity<Page<JobResponseDto>> getAll(@ParameterObject Pageable pageable){
         Page<Job> jobsPage = service.getJobs(pageable);
         Page<JobResponseDto> jobsDtoPage = JobMapper.toResponse(jobsPage);
         return ResponseEntity.ok(jobsDtoPage);
     }
 
 
-
+    @CrossOrigin(origins = "*")
+    @SecurityRequirement(name = "Bearer")
     @GetMapping("/{id}")
     @Operation(summary = "Buscar serviço por ID", description = "Retorna um serviço específico pelo ID fornecido")
     @ApiResponse(responseCode = "200", description = "Serviço encontrado com sucesso")
@@ -43,7 +48,8 @@ public class JobController {
         return ResponseEntity.ok(JobMapper.toResponse(job));
     }
 
-
+    @CrossOrigin(origins = "*")
+    @SecurityRequirement(name = "Bearer")
     @PostMapping
     @Operation(summary = "Criar serviço", description = "Cria um novo serviço com os dados fornecidos")
     @ApiResponse(responseCode = "201", description = "Serviço criado com sucesso")
@@ -52,7 +58,8 @@ public class JobController {
         Job createdJob = service.createService(job);
         return ResponseEntity.status(201).body(JobMapper.toResponse(createdJob));
     }
-
+    @CrossOrigin(origins = "*")
+    @SecurityRequirement(name = "Bearer")
     @PatchMapping("/{id}")
     @Operation(summary = "Atualizar serviço", description = "Atualiza um serviço existente com os dados fornecidos")
     @ApiResponse(responseCode = "200", description = "Serviço atualizado com sucesso")
@@ -62,7 +69,8 @@ public class JobController {
         return ResponseEntity.ok(JobMapper.toResponse(updatedJob));
 
     }
-
+    @CrossOrigin(origins = "*")
+    @SecurityRequirement(name = "Bearer")
     @DeleteMapping("/{id}")
     @Operation(summary = "Deletar serviço", description = "Deleta um serviço existente pelo ID fornecido")
     @ApiResponse(responseCode = "200", description = "Serviço deletado com sucesso")
